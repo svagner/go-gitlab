5f3e8ca20fb8149a11afaa9d29e4071b8bca984c
Stan Putrya <sputrya@intermedia.net>
2017-02-04 18:44:57 +0300

    Add git as executer in go-gitlab

---
+++ git/jobs.go
@@ -0,0 +1,283 @@
+package git
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"time"
+
+	"path/filepath"
+
+	"github.com/howeyc/fsnotify"
+	"github.com/svagner/go-gitlab/config"
+	"github.com/svagner/go-gitlab/logger"
+	git2go "gopkg.in/libgit2/git2go.v22"
+	"../lib/gitter"
+)
+
+
+type SSHConfig struct {
+	PublicKey  []byte
+	PrivateKey []byte
+}
+
+type UpdateHistory struct {
+	Author string
+	Url    string
+}
+
+type GitCommitLog struct {
+	Type        git2go.ObjectType
+	Id          *git2go.Oid
+	IdStr       string
+	Author      GitAuthor
+	Commiter    GitAuthor
+	ParentCount uint
+	TreeId      *git2go.Oid
+	Message     string
+}
+
+type GitBlobLog struct {
+	Type  git2go.ObjectType
+	Id    *git2go.Oid
+	IdStr string
+	Size  int64
+}
+
+type GitTreeLog struct {
+	Type       git2go.ObjectType
+	Id         *git2go.Oid
+	IdStr      string
+	EntryCount uint64
+}
+
+type GitAuthor struct {
+	User    string
+	Email   string
+	Date    time.Time
+	DateStr string
+}
+
+type GitEvents struct {
+	Push   bool
+	Merge  bool
+	Notify bool
+}
+
+type Repository struct {
+	Git	gitter.Gitter
+	Path           string
+	Branch         string
+	Update         chan string
+	Quit           chan bool
+	QuitReport     chan bool
+	Name           string
+	Url            string
+	Lock           bool
+	FileWatchQuit  chan bool
+	fileWatcher    *fsnotify.Watcher
+	FileUpdate     bool
+	Error          bool
+	LastError      string
+	History        []UpdateHistory
+	BlobLog        []GitBlobLog
+	TreeLog        []GitTreeLog
+	CommitLog      GitCommit
+	Events         GitEvents
+	SubDirectories []string
+}
+
+const (
+	DEFAULT_BRANCH = "master"
+)
+
+var (
+	Repositories = make(map[string]*Repository, 0)
+)
+
+type GitCommit []GitCommitLog
+
+// Forward request for length
+func (p GitCommit) Len() int {
+	return len(p)
+}
+
+// Define compare
+func (p GitCommit) Less(i, j int) bool {
+	return p[i].Commiter.Date.Before(p[j].Author.Date)
+}
+
+// Define swap over an array
+func (p GitCommit) Swap(i, j int) {
+	p[i], p[j] = p[j], p[i]
+}
+
+func Init(cfg config.GitConfig, repos map[string]*config.GitRepository) error {
+
+	for _, rep := range repos {
+		var branch string
+		if rep.Branch != "" {
+			branch = rep.Branch
+		} else {
+			branch = DEFAULT_BRANCH
+		}
+
+		log.Println(rep.Remote)
+		logger.DebugPrint("Init new repository (clone) copy for " + rep.Remote + ": " + rep.Path)
+		git, err := gitter.New(cfg)
+		if err != nil {
+			logger.WarningPrint("Error while init git binary: " + err.Error())
+			return err
+		}
+		err = git.Clone(rep.Remote, branch, rep.Path)
+		if err != nil {
+			logger.WarningPrint(fmt.Sprintf("Error while clone git repository %s: %s", rep.Remote, err.Error()))
+			return err
+		}
+		chanQuit := make(chan bool)
+		chanUpdate := make(chan string)
+		chanQuitAccept := make(chan bool)
+		fileWatchQ := make(chan bool)
+		updHist := make([]UpdateHistory, 0)
+		blobLog := make([]GitBlobLog, 0)
+		treeLog := make([]GitTreeLog, 0)
+		cmtLog := make([]GitCommitLog, 0)
+		subDirs := make([]string, 0)
+		Repositories[GitUrl2Orig(rep.Remote)+"/"+rep.Branch] = &Repository{
+			Git:		git,
+			Path:          rep.Path,
+			Branch:        branch,
+			Name:          rep.Remote,
+			Url:           GitOrig2Http(rep.Remote),
+			Quit:          chanQuit,
+			QuitReport:    chanQuitAccept,
+			Update:        chanUpdate,
+			History:       updHist,
+			BlobLog:       blobLog,
+			TreeLog:       treeLog,
+			CommitLog:     cmtLog,
+			FileWatchQuit: fileWatchQ,
+			Events: GitEvents{
+				Push:   rep.PushRequests,
+				Merge:  rep.MergeRequests,
+				Notify: rep.Notifications,
+			},
+			SubDirectories: subDirs,
+		}
+		go Repositories[GitUrl2Orig(rep.Remote)+"/"+rep.Branch].InitFSWatch()
+
+		// get rep log
+		logger.DebugPrint(rep)
+	}
+	return nil
+}
+
+func (rep *Repository) GetUpdates() error {
+	rep.StopFSWatch()
+	err := rep.Git.Pull(rep.Path)
+	if err != nil {
+		rep.StartFSWatch()
+		return err
+	}
+	rep.StartFSWatch()
+	return nil
+}
+
+func GitUrl2Orig(url string) string {
+	repo := strings.SplitN(strings.TrimLeft(url, "ssh://"), "/", 2)
+	return repo[0] + ":" + repo[1]
+}
+
+func GitOrig2Http(url string) string {
+	withoutUser := strings.SplitN(strings.TrimLeft(url, "ssh://"), "@", 2)
+	return "http://" + strings.TrimSuffix(withoutUser[1], ".git")
+}
+
+// File watcher
+
+func (rep *Repository) InitFSWatch() {
+	var err error
+	rep.fileWatcher, err = fsnotify.NewWatcher()
+	if err != nil {
+		logger.CriticalPrint("Filed to initialize file system watcher for <" + rep.Path + ">:" + err.Error())
+	}
+
+	go rep.fsEvent(rep.fileWatcher)
+	<-rep.FileWatchQuit
+	rep.fileWatcher.Close()
+}
+
+func (rep *Repository) StartFSWatch() {
+	if len(rep.SubDirectories) != 0 {
+		logger.WarningPrint("Couldn't remove all subdirectories from watcher. Check it!")
+		return
+	}
+	filepath.Walk(rep.Path, func(pathStr string, info os.FileInfo, err error) error {
+		dir, err := directoryChooser(pathStr, info, err)
+		if err != nil {
+			return err
+		}
+		if dir != "" {
+			rep.SubDirectories = append(rep.SubDirectories, dir)
+		}
+		return nil
+	})
+
+	for _, path := range rep.SubDirectories {
+		err := rep.fileWatcher.Watch(path)
+		logger.DebugPrint("Add directory for watch: " + path)
+		if err != nil {
+			logger.WarningPrint("FS Monitor error monitor path [" +
+				path + "]: " + err.Error())
+		}
+	}
+}
+
+func (rep *Repository) StopFSWatch() {
+	rmCounter := 0
+	for _, path := range rep.SubDirectories {
+		err := rep.fileWatcher.RemoveWatch(path)
+		if err != nil {
+			logger.WarningPrint("Remove directory from watching [" + path +
+				"]: " + err.Error())
+		} else {
+			logger.DebugPrint("Remove directory from watching: " + path)
+			rmCounter++
+		}
+	}
+	if rmCounter == len(rep.SubDirectories) {
+		rep.SubDirectories = make([]string, 0)
+	}
+}
+
+func (rep *Repository) fsEvent(watcher *fsnotify.Watcher) {
+	rep.StartFSWatch()
+	for {
+		select {
+		case ev := <-watcher.Event:
+			if !rep.FileUpdate {
+				rep.Error = true
+				rep.LastError = ev.String()
+				logger.WarningPrint("ALARM! Change repository git without version control! Repository: " + rep.Name + ", Branch: " + rep.Branch + ". Event: " + ev.String())
+				logger.Skype("ALARM! Change repository git without version control! Repository: "+rep.Name+", Branch: "+rep.Branch+". Event: "+ev.String(), "")
+				logger.Slack("ALARM! Change repository git without version control! Repository: "+rep.Name+", Branch: "+rep.Branch+". Event: "+ev.String(), "")
+			}
+		case err := <-watcher.Error:
+			if !rep.FileUpdate {
+				logger.WarningPrint("File watcher exitting... Repository: " + rep.Name + ", Branch: " + rep.Branch + ". Quit: " + err.Error())
+				return
+			}
+		}
+	}
+}
+
+func directoryChooser(pathStr string, info os.FileInfo, err error) (string, error) {
+	if !info.IsDir() {
+		return "", nil
+	}
+	if info.Name() == ".git" {
+		return "", filepath.SkipDir
+	}
+	return pathStr, nil
+}
---
+++ lib/gitter/git.go
@@ -0,0 +1,77 @@
+package gitter
+
+import (
+	"log"
+	"os"
+	"os/exec"
+	"github.com/svagner/go-gitlab/config"
+	"fmt"
+)
+
+// Gitter performs git operations. Works through os/exec.
+type Gitter struct {
+	git string
+	environment string
+}
+
+
+
+// New returns a configured *Gitter or an error.
+func New(cfg config.GitConfig) (*Gitter, error) {
+	g, err := exec.LookPath("git")
+	if err != nil {
+		return nil, err
+	}
+	return &Gitter{
+		git: g,
+		environment: fmt.Sprintf("GIT_SSH=\"/usr/bin/ssh -o StrictHostKeyChecking=no -i %s", cfg.PrivateKey),
+	}, nil
+}
+
+// Clone clones a git repo to a name
+func (g *Gitter) Clone(repo, branch, name string) error {
+	cmd := &exec.Cmd{Path: g.git, Args: []string{"clone", repo, "--branch", branch, "--single-branch", name}, Env: []string{g.environment}}
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
+}
+
+// CheckoutBranch cd's into repoPath and checks out the branch.
+func (g *Gitter) CheckoutBranch(repoPath, branch string) error {
+	curdir, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+	if err = os.Chdir(repoPath); err != nil {
+		return err
+	}
+	defer func() {
+		if err = os.Chdir(curdir); err != nil {
+			log.Print(err)
+		}
+	}()
+	cmd := exec.Command(g.git, "checkout", branch)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
+}
+
+func (g *Gitter) Pull(repoPath string) error {
+	curdir, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+	if err = os.Chdir(repoPath); err != nil {
+		return err
+	}
+	defer func() {
+		if err = os.Chdir(curdir); err != nil {
+			log.Fatal(err)
+		}
+	}()
+	cmd := &exec.Cmd{Path: g.git, Args: []string{"pull"}, Env: []string{g.environment}}
+	cmd := exec.Command(g.git, "pull")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
+}
